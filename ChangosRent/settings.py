069c1f5c3e46b66292da062074c13e7249a56ae8
Leonardo Letang <leonardo.letang@gmail.com>
2024-06-23 15:48:02 -0300

    agrego template con bootstrap para customizar UI. Agrego primer referencia al ui template en la creacion de vehiculo usando datepicker. agrego referencias a los js y css necesarias (sin filtrar por ahora, luego deberiamos usar bloques para no incluir a todas en el template.html) cambios en settings.py

--- ChangosRent/settings.py
+++ ChangosRent/settings.py
@@ -124,7 +124,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/webapp/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "webapp/static/web"),
+    os.path.join(BASE_DIR, "webapp/static/web/template"),
+]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
