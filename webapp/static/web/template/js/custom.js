61a40e591a2f719938d347e9c8224b3f907fe403
Leonardo Letang <leonardo.letang@gmail.com>
2024-06-26 00:20:46 -0300

    agrego menu hamburguesa con las funcionalidades actuales en el template.html base: 	tipos de vehiculos 	vehiculos 	alquileres 	sign out Meto todos los menues posibles juntos,  para ambos roles, hasta manejarlos correctamente en forma separada. MOdifico login para que funcione independiente del menu principal.

--- webapp/static/web/template/js/custom.js
+++ webapp/static/web/template/js/custom.js
@@ -318,7 +318,7 @@ $(document).ready(function () {
       showMethod: "fadeIn",
       hideMethod: "fadeOut",
     };
-    toastr.success("Welcome to Mono Dashboard", "Howdy!");
+    toastr.success("Bienvenido a Changos Rent!!");
   }
 
   if (toaster.length != 0) {
